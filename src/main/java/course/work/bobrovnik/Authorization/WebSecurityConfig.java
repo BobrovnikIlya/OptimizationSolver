d4edc45153039b4aabb572d97a75d3e5004ec136
Ilya Bobrovnik <vanger_like@mail.ru>
2023-12-14 22:15:23 +0300

    Complete login

--- src/main/java/course/work/bobrovnik/Authorization/WebSecurityConfig.java
+++ src/main/java/course/work/bobrovnik/Authorization/WebSecurityConfig.java
@@ -2,17 +2,10 @@ package course.work.bobrovnik.Authorization;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-<<<<<<< HEAD
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
-import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
-=======
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
->>>>>>> origin/master
+
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
